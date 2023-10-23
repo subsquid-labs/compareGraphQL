8fbdcdf6657f813ea9fb34643042f4b7c83d2c79
abernatskiy <abernatskiy@toha.li>
2023-10-24 02:14:10 +0900

    Making ignoring IDs in the temporal test an option
    	modified:   bin/run.js

--- bin/run.js
+++ bin/run.js
@@ -18,11 +18,13 @@ program
 	.argument('<subgraph_url>', 'URL of the subgraph API')
 	.argument('<squid_url>', 'URL of the squid API')
 	.option('-r, --repeats <number>', 'number of records to retrieve in each test', '10')
+	.option('--ignore-ids', 'ignore IDs in the temporal test')
 program.parse()
 
 const subgraphEndpointUrl = program.args[0]
 const squidEndpointUrl = program.args[1]
 const numRecords = parseInt(program.opts().repeats)
+const ignoreIds = !!program.opts().ignoreIds
 
 const { entities: subgraphEntities, nonEntityQueries: subgraphStrayQueries } =
 	parseSchema(getEndpointSchema(subgraphEndpointUrl), 'subgraph')
@@ -60,7 +62,7 @@ const {
 	squidEntities,
 	subgraphEndpointUrl,
 	squidEndpointUrl,
-	{ignoreIds: true, numRecords}
+	{ignoreIds, numRecords}
 )
 if (temporalEntitiesIssues)
 	console.log(`${temporalEntitiesIssues}\n\n---------------------\n`)
